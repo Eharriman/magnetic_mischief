bb4ae9c762d79f259f6759b862d169fb91d7cdb9
Ethan Harriman <61565123+Eharriman@users.noreply.github.com>
2025-03-28 15:43:18 -0400

    spin down test deterministic

--- tests/test_spin_states.py
+++ tests/test_spin_states.py
@@ -4,6 +4,11 @@ from src.physics.spin_state import SpinState, SPIN_UP, SPIN_DOWN, SPIN_X_DOWN, S
 
 class TestSpinState:
     def test_z_measurement(self):
+        # Deterministic z-spin measurement.
         result, collapsed_state = SPIN_UP.measureSpin('z')
         assert np.isclose(result, SpinState.HALF_HBAR)
         assert collapsed_state == SPIN_UP
+
+        #result, collapsed_state = SPIN_DOWN.measureSpin('z')
+        #assert np.isclose(result, -SpinState.HALF_HBAR)
+        #assert collapsed_state == SPIN_DOWN
