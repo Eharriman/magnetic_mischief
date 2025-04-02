fa3b490dbeb146b3f3eeab7f9dc0632637220feb
Ethan Harriman <61565123+Eharriman@users.noreply.github.com>
2025-04-02 15:36:51 -0400

    Declection loop from particle position

--- scripts/sten_gerlach_experiment.py
+++ scripts/sten_gerlach_experiment.py
@@ -4,3 +4,8 @@ magnet = Magnet(gradient=0.5, axis='z')
 
 particles = [Particle([0,0,0], [0,0,1], SPIN_UP), Particle([0,0,0], [0,0,1], SPIN_DOWN)]
 
+for _ in range(100):
+    for particle in particles:
+        magnet.deflect(particle, dt=0.1)
+    print(f"Up: {particles[0].position}, Down: {particles[1].position}")
+
