beb38fea65425b19fa370f943e1259a41fcd83c1
Ethan Harriman <61565123+Eharriman@users.noreply.github.com>
2025-04-01 23:30:12 -0400

    position, velocity and spin attributes

--- src/physics/particles.py
+++ src/physics/particles.py
@@ -2,6 +2,9 @@ from .spin_state import SpinState
 import numpy as np
 
 class Partcile:
-    def __int__(self):
+    def __int__(self, position, velocity, spin_state):
+        self.position = np.array(position, dtype=float)
+        self.velocity = np.array(velocity, dtype=float)
+        self.spin_state = spin_state
 
     def magnetic_moment(self):
