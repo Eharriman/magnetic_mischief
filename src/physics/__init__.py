273135bb1c4d1356ca50fadd8fc298e23b17b94b
Ethan Harriman <61565123+Eharriman@users.noreply.github.com>
2025-04-01 23:41:16 -0400

    physics api

---
+++ src/physics/__init__.py
@@ -0,0 +1,5 @@
+from .spin_state import SpinState, SPIN_UP, SPIN_DOWN, SPIN_X_DOWN, SPIN_X_UP
+from .particles import SpinState
+from .fields import Magnet
+
+__all__ = ['SpinState', 'SPIN_UP', 'SPIN_DOWN', 'SPIN_X_UP', 'SpinState', 'Magnet']
