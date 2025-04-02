4136e2b871eb7d6c33135f7a4b059ad3b4095caf
Ethan Harriman <61565123+Eharriman@users.noreply.github.com>
2025-04-02 15:55:04 -0400

    deflection test on position for z-axis unit test

--- tests/test_deflection.py
+++ tests/test_deflection.py
@@ -3,5 +3,16 @@ import matplotlib.pyplot as plt
 from src.physics import Particle, Magnet, SPIN_UP, SPIN_DOWN
 
 def test_zDeflection():
+    magnet = Magnet(gradient=0.5, axis='z')
+
+    particles = [Particle([0, 0, 0], [0, 0, 0], SPIN_UP), Particle([0, 0, 0], [0, 0, 0], SPIN_DOWN)]
+
+    positions = {'up': [], 'down': []}
+
+    for _ in range(20):
+        for particle in particles:
+            magnet.deflect(particle, dt=0.1)
+        positions['up'].append(particles[0].position[2])
+        positions['down'].append(particles[1].position[2])
 
 def test_xDeflection():
