fc446d9ab7290d70cbc9d2bd6b6bf1e88fcb26e5
Ethan Harriman <61565123+Eharriman@users.noreply.github.com>
2025-04-02 23:19:14 -0400

    Changed a bunch of dumb typos. Runs now

--- scripts/run_deflection_test.py
+++ scripts/run_deflection_test.py
@@ -6,5 +6,5 @@ magnet = Magnet(gradient=0.5, axis='z')
 particles = [Particle([0, 0, 0], [0, 0, 0], SPIN_UP), Particle([0, 0, 0], [0, 0, 0], SPIN_DOWN)]
 
 fig = plotDeflection(particles, magnet, steps=30, dt=0.15)
-fig.savefi('C:/Users/ethan/PycharmProjects/magnetic_mischief/tests/output/z_deflection_demo.png')
-plt.show()
+fig.savefig('C:/Users/ethan/PycharmProjects/magnetic_mischief/tests/output/z_deflection_demo.png')
+fig.show()
